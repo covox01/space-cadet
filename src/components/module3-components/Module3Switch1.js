11a8c6cefd5ab4ff7d74dcb3ed70cdb6983d95e3
Dennis Oba <dennis.oba@publicisna.com>
2019-07-18 12:41:33 -0400

    More progress in animating Module 3

--- src/components/module3-components/Module3Switch1.js
+++ src/components/module3-components/Module3Switch1.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
-import { Back, Elastic, TimelineLite, Power3, TimelineMax } from "gsap";
+import { Back, Elastic, TimelineLite, Power3, TimelineMax, TweenMax } from "gsap";
 import MorphSVGPlugin from "../../MorphSVGPlugin";
 import DrawSVGPlugin from "../../../src/DrawSVGPlugin";
+import { Ease, Power2 } from "gsap/TweenLite";
 
 class Module3Switch1 extends Component {
 	constructor(props) {
@@ -10,6 +11,7 @@ class Module3Switch1 extends Component {
 			animate: false,
 			switchComplete: false,
       };
+      this.confirm = new TimelineMax();
       this.triggerTween = new TimelineLite();
       this.radarCircleTween = new TimelineLite();
       this.morphSVG = MorphSVGPlugin;
@@ -26,25 +28,38 @@ class Module3Switch1 extends Component {
          .to([this.radarCircle, this.filter2], .5, {
             attr: { stdDeviation: 15},
             fill: "#f9e815",
-         }, "-=.2")
+         }, "-=.4")
+         .to([this.innerCircle, this.filter3], .5, {
+            attr: { stdDeviation: 15},
+            fill: "#a3e8ce"
+         }, "-=.4")
+         .to([this.innerCircle], .5, {
+            transformOrigin: "(150% 50%)",
+            rotation: -90
+          }, "-=.4")
    }
 
-   switchOn = () => {
+   sequenceOne = () => {
     this.triggerTween
        .to([this.filter, this.trigger2], .5, {
           attr: { stdDeviation: 15},
           fill: "#ef1c00",
           ease: Power3.easeOut,
-          onComplete: this.sequence1
        })
+       this.sequence1()
     }
    
-    switchOff = () => {
+    sequenceTwo = () => {
         this.triggerTween
            .to([this.filter, this.innerCircle], .5, {
-              attr: { stdDeviation: 0},
-              fill: "#808080",
+              transformOrigin: "(150% 50%)",
+              rotation: -180,
+            //   fill: "#f9e815",
               ease: Power3.easeOut
+           })
+           .to([this.filter4, this.outerCircle], .5, {
+              attr: { stdDeviation: 15},
+              fill: "#a3e8ce"
            })
       }
 
@@ -63,17 +78,27 @@ class Module3Switch1 extends Component {
     }
 
 	handleClick = () => {
+      this.confirm
+         .to([this.trigger1], .3, {
+            fill: "#282828",
+            ease: Power2.easeInOut
+         })
+         .to([this.trigger1], .3, {
+            fill: "#303030",
+            ease: Power2.easeInOut
+         })
+
       this.handleSwitch();
       if (!this.state.animate) {
          this.setState({
             animate: true
          })
-         this.switchOn();
+         this.sequenceOne();
       } else {
          this.setState({
             animate: false
          })
-         this.switchOff()
+         this.sequenceTwo()
       }
    }
 
@@ -104,12 +129,16 @@ class Module3Switch1 extends Component {
                      id="am3-radar-face"
                   />
                   <path
+                     ref={path => (this.outerCircle = path)}
+                     filter="url(#outerCircleFilter)"
                      id="am3-radar-outer-ring"
                      fill="gray"
                      d="M568.9,500.5c-117,0-212.2-95.2-212.2-212.2S451.9,76,568.9,76s212.2,95.2,212.2,212.2 S686,500.5,568.9,500.5z M568.9,79.7c-115,0-208.5,93.5-208.5,208.5S454,496.7,568.9,496.7s208.5-93.5,208.5-208.5 S683.9,79.7,568.9,79.7z"
                      id="am3-outer-ring"
                   />
                   <path
+                     ref={path => (this.innerCircle = path)}
+                     filter="url(#innerCircleFilter)"
                      id="am3-radar-inner-ring"
                      fill="gray"
                      d="M778,285.4H661.2c-1.5-49.7-42.2-89.4-92.2-89.4c-50,0-90.7,39.9-92.2,89.4H359.9	v5.6h116.9c1.5,49.7,42.2,89.4,92.2,89.4c50,0,90.7-39.9,92.2-89.4H778V285.4z M568.9,201.6c46.9,0,85.1,37.3,86.6,83.8H482.3	C483.8,238.9,522.1,201.6,568.9,201.6z M568.9,375c-46.9,0-85.1-37.3-86.6-83.8h173.2C654.1,337.5,615.8,375,568.9,375z"
@@ -143,6 +172,36 @@ class Module3Switch1 extends Component {
                         <feOffset in="SourceGraphic" dx="0" dy="0" />
                         <feGaussianBlur
                            ref={filter => (this.filter2 = filter)}
+                           result="blurOut"
+                           in="offOut"
+                           stdDeviation="0"
+                        />
+                        <feBlend
+                           in="SourceGraphic"
+                           in2="blurOut"
+                           mode="normal"
+                           opacity="0"
+                        />
+                     </filter>
+                     <filter id="innerCircleFilter" x="-2" y="-.8" width="400%" height="300%">
+                        <feOffset in="SourceGraphic" dx="0" dy="0" />
+                        <feGaussianBlur
+                           ref={filter => (this.filter3 = filter)}
+                           result="blurOut"
+                           in="offOut"
+                           stdDeviation="0"
+                        />
+                        <feBlend
+                           in="SourceGraphic"
+                           in2="blurOut"
+                           mode="normal"
+                           opacity="0"
+                        />
+                     </filter>
+                     <filter id="outerCircleFilter" x="-2" y="-.8" width="400%" height="300%">
+                        <feOffset in="SourceGraphic" dx="0" dy="0" />
+                        <feGaussianBlur
+                           ref={filter => (this.filter4 = filter)}
                            result="blurOut"
                            in="offOut"
                            stdDeviation="0"
